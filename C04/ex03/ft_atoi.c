89039bd5a470f8c3aa20413e76e2a025309c0189
Samir <smrcheese@gmail.com>
2023-09-13 16:23:57 +0100

    vfixed

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,35 +6,34 @@
 /*   By: sezequie <sezequie@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 01:50:30 by sezequie          #+#    #+#             */
-/*   Updated: 2023/09/11 16:28:01 by sezequie         ###   ########.fr       */
+/*   Updated: 2023/09/13 16:21:33 by sezequie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_atoi(char *str)
+int    ft_atoi(char *str)
 {
-	int	i;
-	int	n;
-	int	sign;
+    int    num;
+    int    i;
+    int    sign;
 
-	i = 0;
-	n = 0;
-	sign = 1;
-	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' \
-			|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
-		i++;
-	while ((str[i] == '-' || str[i] == '+') && (str[i + 1] == '-' \
-			|| str[i + 1] == '+'))
-		return (0);
-	if (str[i] == '-')
-		sign = -1;
-	if (str[i] == '-' || str[i] == '+')
-		i++;
-	if ((str[i] < '0') || (str[i] > '9'))
-		return (0);
-	while ((str[i] >= '0') && (str[i] <= '9'))
-	{
-		n = n * 10 + (str[i] - '0');
-		i++;
-	}
-	return (n * sign);
+    i = 0;
+    sign = 1;
+    num = 0;
+    while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+        i++;
+    while (str[i] == '-' || str[i] == '+')
+    {
+        if (str[i] == '-')
+        {
+            sign *= -1;
+        }
+        i++;
+    }
+    while (str[i] >= 48 && str[i] <= 57)
+    {
+        num *= 10;
+        num += str[i] - 48;
+        i++;
+    }
+    return (num * sign);
 }
