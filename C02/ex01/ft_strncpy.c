2f8196938477e81aa2e5c005d2fb155f11fcc8d4
Samir <smrcheese@gmail.com>
2023-09-13 13:03:21 +0100

    modified

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -6,21 +6,28 @@
 /*   By: sezequie <sezequie@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 21:03:05 by sezequie          #+#    #+#             */
-/*   Updated: 2023/09/12 15:06:26 by sezequie         ###   ########.fr       */
+/*   Updated: 2023/09/13 13:02:53 by sezequie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int	i;
+	unsigned int	index;
 
-	i = 0;
-	while (i < n - 1 && src[i] != '\0')
+	index = 0;
+	while (src[index] != '\0' && index < n)
 	{
-		dest[i] = src[i];
-		i++;
+		dest[index] = src[index];
+		index++;
 	}
-	dest[i] = '\0';
+	if (index < n)
+	{
+		while (index < n)
+		{
+			dest[index] = '\0';
+			index++;
+		}
+	}
 	return (dest);
 }
 
