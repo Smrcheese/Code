3b295436a3efbe71a918ae213430e7ef2ccfc1a3
Samir <smrcheese@gmail.com>
2023-09-13 13:16:52 +0100

    added ex02

---
+++ C07/ex02/ft_ultimate_range.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sezequie <sezequie@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/13 13:15:56 by sezequie          #+#    #+#             */
+/*   Updated: 2023/09/13 13:16:11 by sezequie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	long int	i;
+	int			*ptr;
+
+	if (min >= max)
+	{
+		*range = NULL;
+		return (0);
+	}
+	ptr = malloc((max - min) * sizeof(int));
+	if (ptr == NULL)
+		return (-1);
+	i = 0;
+	while (min < max)
+	{
+		ptr[i] = min;
+		min++;
+		i++;
+	}
+	*range = ptr;
+	return (i);
+}
